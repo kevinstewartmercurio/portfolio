73e4432d02ed76e862fc63f848e105d7d1d4aae3
kstewar1 <77819538+kstewar1@users.noreply.github.com>
2023-03-06 12:14:02 -0500

    ready to add content to projects preview section

---
+++ src/components/ProjectsPreview.jsx
@@ -0,0 +1,15 @@
+export function ProjectsPreview() {
+    return (
+        <>
+            <div className="border-solid border-2 border-red-300 box-border bg-[#212f1d] w-full h-[200px] pt-8 absolute z-10">
+                {/* <h1 className="font-['Poppins'] text-5xl md:text-7xl lg:text-8xl font-bold">
+                    projects
+                </h1> */}
+            </div>
+            <div className="bg-[url(/waves-haikei.png)] bg-no-repeat w-full h-[128px] mt-[160px] absolute"></div>
+            <div className="border-solid border-2 border-blue-300 box-border bg-[#703f01] w-full h-[200px] mt-[288px] pb-8 absolute z-10">
+
+            </div>
+        </>
+    )
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,13 @@
-import { Welcome } from "@/components/Welcome"
-import { Bio } from "@/components/Bio"
+import { Welcome } from "@/components/Welcome";
+import { Bio } from "@/components/Bio";
+import { ProjectsPreview } from "@/components/ProjectsPreview";
 
 export default function Home() {
-  return (
-    <>
-      <Welcome/>
-      <Bio/>
-    </>
-  )
+    return (
+        <>
+            <Welcome />
+            <Bio />
+            <ProjectsPreview />
+        </>
+    );
 }
