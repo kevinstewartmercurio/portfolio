790a1c81ab20a9a4e87979abe4c099057e572a1c
kstewar1 <77819538+kstewar1@users.noreply.github.com>
2023-03-05 20:55:42 -0500

    added layered peaks

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <>
-      
+      <div className="border-solid border-2 border-blue-700 box-border bg-[url(/layered-peaks-haikei.png)] bg-cover bg-no-repeat h-[320px] rotate-180"></div>
     </>
   )
 }
