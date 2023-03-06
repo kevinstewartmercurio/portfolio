7f38c6c37a8c79d65da448fcdd1c4cddbabf38c0
kstewar1 <77819538+kstewar1@users.noreply.github.com>
2023-03-05 23:00:59 -0500

    added intro section, added responsive styling

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -7,6 +7,7 @@ export default function Document() {
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400&family=Poppins:wght@400;600;700&display=swap" rel="stylesheet" />
+        <script src="https://kit.fontawesome.com/f06830f55e.js" crossorigin="anonymous"></script>
       </Head>
       <body>
         <Main />
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,66 @@
+export const buttons = [
+  {
+    id: "github-btn", 
+    iconClassName: "fa-brands fa-github text-2xl",
+    url: "https://github.com/kstewartmercurio"
+  },
+  {
+    id: "linkedin-btn", 
+    iconClassName: "fa-brands fa-linkedin text-2xl",
+    url: "https://www.linkedin.com/in/kevin-stewart-mercurio-3a8817246/"
+  }
+]
+
 export default function Home() {
   return (
     <>
-      <div className="border-solid border-2 border-blue-700 box-border bg-[url(/layered-peaks-haikei.png)] bg-cover bg-no-repeat h-[320px] rotate-180"></div>
+      {/* <div className="bg-[#6b7c66] h-[580px] pl-[18%] pt-[6%]"> */}
+      <div className="bg-[#6b7c66] h-[360px] md:h-[470px] lg:h-[580px] pl-[8%] md:pl-[10%] lg:pl-[14%] pr-[2%] pt-[12%] sm:pt-[10%] md:pt-[12%] lg:pt-[8%]">
+        {/* <h2 className="text-[#cac9c9] font-['Inter'] text-7xl leading-[84px]"> */}
+        <h2 className="text-[#cac9c9] font-['Inter'] text-4xl md:text-5xl lg:text-7xl leading-[42px] md:leading-[64px] lg:leading-[84px]">
+          Hi, my name is
+        </h2>
+        {/* <div className="text-7xl leading-[84px]"> */}
+        <div className="text-4xl md:text-5xl lg:text-7xl leading-[42px] md:leading-[64px] lg:leading-[84px]">
+          <span className="text-[#703f01] font-['Poppins']">
+            Kevin Stewart-Mercurio
+          </span>
+          <spspan className="text-[#cac9c9] font-['Inter']">
+            .
+          </spspan>
+        </div>
+        {/* <div className="text-[#cac9c9] mt-8 font-['Inter'] text-2xl"> */}
+        <div className="text-[#cac9c9] mt-8 font-['Inter'] text-xl lg:text-2xl">
+          <span>
+            I'm a&nbsp;
+          </span>
+          <span className="text-[#703f01]">
+            software developer&nbsp;
+          </span>
+          <span>
+            and I'm ready to work with you.
+          </span>
+        </div>
+        {/* <div className="mt-4 pl-[36%]"> */}
+        <div className="mt-4 pl-[48%] sm:pl-[64%] md:pl-[56%] lg:pl-[36%]">
+          {buttons.map((el, idx) => {
+            return (
+              <button 
+                key={idx}
+                id={el.id}
+                // className="text-[#cac9c9] bg-[#6b7c66] w-10 h-10 ml-[1%] border-[1.5px]  border-solid border-[#cac9c9] rounded-full"  
+                className="text-[#cac9c9] bg-[#6b7c66] w-10 h-10 ml-[4%] lg:ml-[1%] border-[1.5px]  border-solid border-[#cac9c9] rounded-full"  
+                onClick={() => {
+                  window.open(el.url, "_blank")
+                }}
+              >
+                <i className={el.iconClassName}></i>
+              </button>
+            )
+          })}
+        </div>
+      </div>
+      <div className="bg-[url(/layered-peaks-haikei.png)] bg-cover bg-no-repeat h-[320px] rotate-180"></div>
     </>
   )
 }
